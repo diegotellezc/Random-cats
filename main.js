791f6044fa47a36b1e8412289546f6c509e8f552
Diego Tellez <diegotellezcontreras@gmail.com>
2022-11-22 17:23:58 -0500

    Primer commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const URL = "https://api.thedogapi.com/v1/images/search"
+
+// fetch(URL)
+//     .then(res => res.json())
+//     .then(data => {
+//         const IMG = document.querySelector("img")
+//         IMG.src = data[0].url
+//     })
+
+async function fetchDog () {
+    const res = await fetch(URL)
+    const data = await res.json()
+    const img = document.querySelector("img")
+    img.src = data[0].url
+}
+
+const button = document.querySelector("button")
+button.onclick = fetchDog
